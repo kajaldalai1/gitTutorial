41ea2cd83473966c144e066f105f73fa047a845a
Kajal Dalai <kajaldalai1@gmail.com>
2023-09-21 02:12:11 +0530

    my name gets printed in the server

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(4000, () => {
-  console.log('Server is running on port 4000');
+  console.log('Server is running!! on port 4000');
 });
