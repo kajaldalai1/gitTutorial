7fac71633979397e61e6d27fdf60345959083f97
Kajal Dalai <kajaldalai1@gmail.com>
2023-11-15 19:52:33 +0530

    Making SQL easy with Sequelize

--- express js/Retriving data from data bases/controllers/shop.js
+++ express js/Retriving data from data bases/controllers/shop.js
@@ -1,41 +1,47 @@
 const Product = require('../models/product');
 const Cart = require('../models/cart');
-exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-  .then(([rows,fieldData])=>{
+
+exports.getProducts = async (req, res, next) => {
+  try {
+    const [products, _] = await Product.fetchAll();
     res.render('shop/product-list', {
-      prods: rows,
+      prods: products,
       pageTitle: 'All Products',
       path: '/products'
     });
-  })
-  .catch(err=>{console.log(err)});
-  
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
 };
 
-exports.getProduct = (req,res,next)=>{
+exports.getProduct = async (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId)
-  .then(([product]) =>{
+  try {
+    const [product] = await Product.findById(prodId);
     res.render('shop/product-detail', {
-      product:product[0], 
-      pageTitle: product.title,
+      product: product[0],
+      pageTitle: product[0].title,
       path: '/products'
     });
-  })
-  .catch(err=> console.log(err));
-}
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+};
 
-exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-  .then(([rows,fieldData])=>{
+exports.getIndex = async (req, res, next) => {
+  try {
+    const [products, _] = await Product.fetchAll();
     res.render('shop/index', {
-      prods: rows,
+      prods: products,
       pageTitle: 'Shop',
       path: '/'
     });
-  })
-  .catch(err=>{console.log(err)});
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
 };
 
 exports.getCart = (req, res, next) => {
@@ -45,15 +51,19 @@ exports.getCart = (req, res, next) => {
   });
 };
 
-exports.postCart = (req,res,next)=>{
+exports.postCart = async (req, res, next) => {
   const prodId = req.body.productId;
   console.log(prodId);
-  Product.findById(prodId,(product)=>{
-    Cart.addProduct(prodId,product.price);
-
-  });
-  res.redirect('/cart');
+  try {
+    const [product] = await Product.findById(prodId);
+    Cart.addProduct(prodId, product.price);
+    res.redirect('/cart');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
 };
+
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
     path: '/orders',
